22fff2c932541f5e3d61d2293d7405e97b6eac0f
David Murillo <d.murillo28@yahoo.com>
2020-05-23 18:07:25 -0700

    Initial Commit

---
+++ LyftClone/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LyftClone
+//
+//  Created by David Murillo on 5/23/20.
+//  Copyright © 2020 MuriTech. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
